3d2689fbc747b1604f4cf9693201db2f3f54478c
ChloeBeckert <chloebeckert822@gmail.com>
2021-09-02 11:23:54 -0500

    hi

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,13 +1,19 @@
-var f1;
+let f1, f2;
 function setup() {
   createCanvas(1000, 1000);
   f1= loadFont("assets/Mindset.ttf")
+  f2= loadFont("assets/Rich.otf")
+  textAlign(CENTER);
 }
 
 function draw() {
   background("white");
   textFont(f1);
-  textSize(120);
-  text("Hello World!", 250,250);
+  textSize(48);
+  text("Hello World!", width/2 ,300);
+
+  textFont(f2);
+  textSize(48)
+  text("Go you Redbirds!", width/2, 150)
 
 }
